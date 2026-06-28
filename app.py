95fbb905227aafcad395105e81321478e71816f6
khadsesakshi <khadsesakshi03@gmail.com>
2026-06-28 17:25:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to CICD Project"
+    return "Welcome to Flask CICD Project"
 
 if  __name__ == "__main__":
     app.run(debug=True)
