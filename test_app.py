6ecd00588852919481e916f13f2188d585745dc2
khadsesakshi <khadsesakshi03@gmail.com>
2026-06-29 10:47:14 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -3,9 +3,9 @@ from app import app
 def test_home_page():
       
     tester = app.test_client()
-
+      
     response = tester.get("/")
 
     assert response.status_code == 200
-
+      
     assert b"Welcome to CICD Project" in response.data
